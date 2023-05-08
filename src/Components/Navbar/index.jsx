64ef10ca45aed930b08b278f45afee075ef68ab4
ARADHYA SRIVASTAVA <aradhyasrivastava0504@gmail.com>
2023-05-08 23:34:27 +0530

    Add files via upload

---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,34 @@
+import styles from "./styles.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.nav_container}>
+      <div className={styles.nav_left}>
+        <a href="#home">Aradhya</a>
+      </div>
+      <div className={styles.nav_right}>
+        <a href="#about" className={styles.nav_link}>
+          <span>01.</span>About
+        </a>
+        {/* <a href="#experience" className={styles.nav_link}>
+          <span>02.</span>Experience
+        </a> */}
+        <a href="#work" className={styles.nav_link}>
+          <span>02.</span>Work
+        </a>
+        <a href="#contact" className={styles.nav_link}>
+          <span>03.</span>Contact
+        </a>
+        <a
+          href="https://drive.google.com/file/d/18NjNFNMt8K-l4NeEIHkWSbLskT4utJ9_/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className={styles.nav_resume_btn}>Resume</button>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
